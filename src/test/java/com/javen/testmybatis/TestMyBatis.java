9574601422b7e6681cc9669add2e909ee12f1237
donald <809077529@qq.com>
2018-04-12 08:50:40 +0800

    f

--- src/test/java/com/javen/testmybatis/TestMyBatis.java
+++ src/test/java/com/javen/testmybatis/TestMyBatis.java
@@ -11,26 +11,17 @@ import com.alibaba.fastjson.JSON;
 import com.javen.model.User;
 import com.javen.service.IUserService;
 
-@RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 
 public class TestMyBatis {
     private static Logger logger = Logger.getLogger(TestMyBatis.class);
-    //  private ApplicationContext ac = null;
     @Resource
     private IUserService userService = null;
-
-//  @Before  
-//  public void before() {  
-//      ac = new ClassPathXmlApplicationContext("applicationContext.xml");  
-//      userService = (IUserService) ac.getBean("userService");  
-//  }  
 
     @Test
     public void test1() {
         User user = userService.getUserById(2);
-        // System.out.println(user.getUserName());  
-        // logger.info("值："+user.getUserName());  
         logger.info(JSON.toJSONString(user));
     }
 }
